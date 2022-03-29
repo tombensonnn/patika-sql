20e21ed43360d3c001d9c89a7b0ba034d7edb30e
Can UYUMAZ <58149556+tombensonnn@users.noreply.github.com>
2022-03-29 16:01:12 +0300

    Update patika_odev_5.sql

--- patika_odev_5.sql
+++ patika_odev_5.sql
@@ -16,6 +16,6 @@ OFFSET 5;
 -- 3- customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
 
 SELECT * FROM customer
-WHERE stode_id = 1
+WHERE store_id = 1
 ORDER BY last_name DESC
 LIMIT 4;
